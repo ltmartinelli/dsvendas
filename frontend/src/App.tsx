5b3a3f3990bff63891fb3f281dc428487c68f165
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-09 17:02:10 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,33 @@
+import BarChart from "./components/BarChart";
 import DataTable from "./components/DataTable";
+import DonutChart from "./components/DonutChart";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
-function App() {
+
+function App()
+{
 
   return (
     <>
-    <NavBar/>
-    <DataTable/> 
-    <Footer/>
-    </>  
+      <NavBar />
+      <section className="container">
+        <h1 className="dsv-text-primary py-3">Dashboard</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Participação nas vendas (%)</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <h2 className="py-3 dsv-text-primary">Tabela de Vendas</h2>
+        <DataTable />
+      </section>
+      <Footer />
+    </>
   )
 }
 
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,9 @@ export default function Footer()
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">
-                    GitHub: <a href="https://github.com/ltmartinelli" target="_blank" rel="noreferrer">/ltmartinelli</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ltmartinelli" target="_blank" rel="noreferrer">Lucas Martinelli</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
